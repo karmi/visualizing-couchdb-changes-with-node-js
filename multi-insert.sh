1d94044d73bac09f8030d275765a08d49f453b3e
Karel Minarik <karmi@karmi.cz>
2010-11-20 10:39:02 +0100

    Adapted the shell script to insert multiple documents

--- multi-insert.sh
+++ multi-insert.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for i in {1..5}
+echo -e "\n\e[1mInserting 500 documents into http://localhost:5984/test. Stop me with Ctrl+C.\e[0m\n\n"
+
+for i in {1..5000}
   do
     curl -s -H 'Content-Type: application/json' -X POST http://localhost:5984/test -d '{"foo":"bar"}'
   done
